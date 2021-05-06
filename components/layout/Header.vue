1a416bbf5c50aec2dac6b967738f4ae4fa129900
qianyuan <694523813@qq.com>
2021-05-06 19:03:40 +0800

    start

---
+++ components/layout/Header.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-app-bar elevate-on-scroll fixed app color="white">
+    <v-spacer></v-spacer>
+    <v-toolbar-title>Title</v-toolbar-title>
+    <v-spacer></v-spacer>
+  </v-app-bar>
+</template>
